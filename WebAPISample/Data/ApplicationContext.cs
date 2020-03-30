3c40496542966bed872582b1b855699606ada0ee
unknown <shaelappen@gmail.com>
2020-03-30 13:52:02 -0500

    Seeded database

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -3,21 +3,28 @@ using WebAPISample.Models;
 
 namespace WebAPISample.Data
 {
+
     public class ApplicationContext : DbContext
     {
         public ApplicationContext(DbContextOptions options)
-            :base(options)
+            : base(options)
         {
 
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            // Seed data - needs migration
-           
-        }
-
         public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Seed data - needs migration
+            builder.Entity<Movie>()
+                 .HasData(
+                new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" },
+                new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" },
+                new Movie { MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" }
+             );
+        }
     }
+
 }
