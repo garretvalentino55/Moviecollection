b4587626d8992b322ce1fb14bf5c6c9c84433e64
unknown <shaelappen@gmail.com>
2020-04-01 16:24:56 -0500

    delete functionality implemented

--- Client/app.js
+++ Client/app.js
@@ -28,6 +28,109 @@
 
 
 
+
+
+function del(movieId){
+        $.ajax({
+            url: "https://localhost:44325/api/movie/"+ movieId,
+                dataType: 'json',
+                type: 'delete',
+                contentType: 'application/json',
+                data: JSON.stringify(movieId),
+                success: function(){
+                console.log("Success")
+    }
+})
+}
+
+
+// $(function(){
+//     $.del(movieId)
+
+//     $.ajax({
+//         url: "https://localhost:44325/api/movie/"+ movieId,
+//         data: JSON.stringify(data),
+//         success: function(data){
+//             $("#MovieInfo").`
+//                 `);
+//         }
+//     })
+// }
+
+
+// $(function del(movieId){
+//     $.ajax({
+//         url: "https://localhost:44325/api/movie/"+ movieId,
+//         type: 'delete'
+
+
+// })
+// })
+
+
+
+$(function(){
+    $.get("https://localhost:44325/api/movie", function(data){
+        {
+        data.map(function(el){
+            $("#Movies").append(`
+            <tr id = "${(el.movieId)}">
+            <td>${(el.title)}</td>
+            <td>${(el.director)}</td>
+            <td>${(el.genre)}</td>
+
+            <td>
+            <button onClick="edit(${(el.movieId)})">Edit</button>
+            <button onClick="del(${(el.movieId)})">Delete</button>
+            </td>
+            </tr>
+
+            `)
+        })
+        }
+    })
+})
+
+
+
+// $(function(){
+//     $.delete(movieId)
+//     data = {};
+//     $.ajax({
+//         url: "https://localhost:44325/api/movie/"+ movieId,
+//         data: JSON.stringify(data),
+//         type: 'delete'
+//         }
+//     )
+// })
+
+// $(function(){
+// $.get(), function(data){
+
+// }
+// }
+
+// $(function(){
+//     var tbody = document.getElementById("tbody");
+//     var row = tbody.row.insertRow()
+//     var title = row.insertCell1();
+//     var director = row.insertCell2();
+//     var genre = row.insertCell3();
+//     cell1.innerHTML = 
+
+
+    // var movie = {
+    //     movieId: this["movieId"].value
+    // }
+
+
+
+// function PostAction(){
+//     $.post()
+// }
+
+
+
 // $(function(){
 
 //     data = {};
@@ -50,64 +153,3 @@
 // <td>${JSON.stringify(data[i]['genre'])}</td>
 // </tr>
 // `)
-
-
-
-
-$(function(){
-    $.get("https://localhost:44325/api/movie", function(data){
-        {
-        data.map(function(el){
-            $("#Movies").append(`
-            <tr>
-            <td>${(el.title)}</td>
-            <td>${(el.director)}</td>
-            <td>${(el.genre)}</td>
-            </tr>
-            `)
-        })
-        }
-    })
-})
-
-// $(function(){
-// $.get(), function(data){
-
-// }
-// }
-
-// $(function(){
-//     var tbody = document.getElementById("tbody");
-//     var row = tbody.row.insertRow()
-//     var title = row.insertCell1();
-//     var director = row.insertCell2();
-//     var genre = row.insertCell3();
-//     cell1.innerHTML = 
-
-
-    // var movie = {
-    //     movieId: this["movieId"].value
-    // }
-$(function(){
-
-    var id = 1;
-    data = {};
-    $.ajax({
-        url: "https://localhost:44325/api/movie/"+ id,
-        data: JSON.stringify(data),
-        success: function(data){
-            $("#MovieInfo").append(`<div> MovieTitle: ${data["title"]}</div>
-                 <div> Director: ${data["director"]}</div>
-                 <div> Genre: ${data["genre"]}</div>
-                `);
-        }
-    })
-})
-
-
-// function PostAction(){
-//     $.post()
-// }
-
-
-
