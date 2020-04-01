4d4374e39865d263fdb87516ba092c0b685ceb78
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-04-01 16:39:28 -0500

    this is with some work in the edit it was just something i thought i would try

--- Client/app.js
+++ Client/app.js
@@ -63,6 +63,7 @@ $(function(){
             <td>${(el.title)}</td>
             <td>${(el.director)}</td>
             <td>${(el.genre)}</td>
+            <td><button type="submit">Edit</button></td>
             </tr>
             `)
         })
@@ -106,3 +107,34 @@ $(function(){
         }
     })
 })
+
+
+
+(function($){
+    function processForm( f ){
+        var dict = {
+        	Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["input"].value
+        }; 
+
+        
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        f.preventDefault();
+    }
+
+    $('#my-form').edit( processForm );
+})(jQuery);
